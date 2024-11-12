21d28847697c333760fd884454138017f1408aed
Tony Kay <tony.kay@gmail.com>
2024-11-11 18:00:06 -0800

    Updated example to include a parallel node for routes.

--- src/main/com/example/client.cljs
+++ src/main/com/example/client.cljs
@@ -24,6 +24,7 @@
     [com.fulcrologic.statecharts.chart :refer [statechart]]
     [com.fulcrologic.statecharts.data-model.operations :as ops]
     [com.fulcrologic.statecharts.elements :as ele :refer [on-entry parallel script state transition]]
+    [com.fulcrologic.statecharts.integration.fulcro.ui-routes-options :as ro]
     [taoensso.timbre :as log]
     [taoensso.tufte :as tufte :refer [profile]]))
 
@@ -37,22 +38,34 @@
 
 (defsc Root [this props]
   {:query [(scf/statechart-session-ident uir/session-id)]}
-  (div :.ui.container
-    (h2 "Root")
-    (dom/pre {}
-      (str
-        (with-out-str
-          (pprint (comp/get-query this)))
-        "\n"
-        (scf/current-configuration this uir/session-id)))
-    (div :.ui.segment
-      (div :.ui.items
-        (div :.item {:classes []
-                     :onClick (fn [] (uir/route-to! this `RouteA1))} "Goto A1")
-        (div :.item {:classes []
-                     :onClick (fn [] (uir/route-to! this `RouteA2))} "Goto A2"))
+  (let [route-denied? (uir/route-denied? this)]
+    (div :.ui.container
+      (h2 "Root")
+      (dom/pre {}
+        (str
+          (with-out-str
+            (pprint (comp/get-query this)))
+          "\nConfig: "
+          (with-out-str
+            (pprint
+              (scf/current-configuration this uir/session-id)))
+          "\nActive leaves: "
+          (uir/active-leaf-routes this)))
+      (when route-denied?
+        (div :.ui.error.message
+          "The route was denied because it is busy! "
+          (dom/a {:onClick (fn []
+                             (uir/force-continue-routing! this))} "Route anyway!")))
       (div :.ui.segment
-        (uir/ui-current-subroute this)))))
+        (div :.ui.items
+          (div :.item {:classes []
+                       :onClick (fn [] (uir/route-to! this `RouteA1))} "Goto A1")
+          (div :.item {:classes []
+                       :onClick (fn [] (uir/route-to! this `RouteA2))} "Goto A2")
+          (div :.item {:classes []
+                       :onClick (fn [] (uir/route-to! this `RouteA3))} "Goto A3"))
+        (div :.ui.segment
+          (uir/ui-current-subroute this comp/factory))))))
 
 (defsc RouteA1 [this {:ui/keys [clicks] :as props}]
   {:query         [:ui/clicks]
@@ -69,7 +82,12 @@
    :ident         (fn [] [:component/id ::RouteA2])}
   (div :.ui.basic.container
     (h3 "Route A2")
-    (uir/ui-current-subroute this)))
+    (div :.ui.items
+      (div :.item {:classes []
+                   :onClick (fn [] (uir/route-to! this `RouteA21))} "Goto A21")
+      (div :.item {:classes []
+                   :onClick (fn [] (uir/route-to! this `RouteA22))} "Goto A22"))
+    (uir/ui-current-subroute this comp/factory)))
 
 (defsc RouteA21 [this {:ui/keys [clicks] :as props}]
   {:query         [:ui/clicks]
@@ -83,9 +101,40 @@
 (defsc RouteA22 [this {:ui/keys [clicks] :as props}]
   {:query         [:ui/clicks]
    :initial-state {:ui/clicks 0}
+   ro/busy?       (fn [& args] true)
    :ident         (fn [] [:component/id ::RouteA22])}
   (div :.ui.basic.container
     (h3 "Route A22")
+    (button {:onClick (fn [] (m/set-integer! this :ui/clicks :value (inc clicks)))}
+      (str clicks))))
+
+(defsc RouteA3 [this {:ui/keys [clicks] :as props}]
+  {:query         [:ui/clicks]
+   :initial-state {:ui/clicks 0}
+   :ident         (fn [] [:component/id ::RouteA3])}
+  (div :.ui.basic.container
+    (h3 "Route A3")
+    (div :.ui.grid
+      (div :.eight.wide.column
+        (uir/ui-parallel-route this `RouteA31 comp/factory))
+      (div :.eight.wide.column
+        (uir/ui-parallel-route this `RouteA32 comp/factory)))))
+
+(defsc RouteA31 [this {:ui/keys [clicks] :as props}]
+  {:query         [:ui/clicks]
+   :initial-state {:ui/clicks 0}
+   :ident         (fn [] [:component/id ::RouteA31])}
+  (div :.ui.basic.container
+    (h3 "Route A31")
+    (button {:onClick (fn [] (m/set-integer! this :ui/clicks :value (inc clicks)))}
+      (str clicks))))
+
+(defsc RouteA32 [this {:ui/keys [clicks] :as props}]
+  {:query         [:ui/clicks]
+   :initial-state {:ui/clicks 0}
+   :ident         (fn [] [:component/id ::RouteA32])}
+  (div :.ui.basic.container
+    (h3 "Route A32")
     (button {:onClick (fn [] (m/set-integer! this :ui/clicks :value (inc clicks)))}
       (str clicks))))
 
@@ -97,7 +146,11 @@
         (uir/rstate {:route/target `RouteA1})
         (uir/rstate {:route/target `RouteA2}
           (uir/rstate {:route/target `RouteA21})
-          (uir/rstate {:route/target `RouteA22}))))))
+          (uir/rstate {:route/target `RouteA22}))
+        (uir/rstate {:parallel?    true
+                     :route/target `RouteA3}
+          (uir/rstate {:route/target `RouteA31})
+          (uir/rstate {:route/target `RouteA32}))))))
 
 (defn refresh []
   ;; hot code reload of installed controls
